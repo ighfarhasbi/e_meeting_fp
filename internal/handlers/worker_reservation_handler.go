2e428aaae6de43c8a28c20f2d24319f2087787d5
ighfarhasbi <ighfarhasbiash@gmail.com>
2025-08-13 09:04:28 +0700

    feat: unit test (race condition) API reservation

--- internal/handlers/worker_reservation_handler.go
+++ internal/handlers/worker_reservation_handler.go
@@ -16,14 +16,16 @@ func WorkerReservation(rdb *redis.Client, dbConn *sql.DB) {
 	fmt.Println("Worker started, waiting for jobs...")
 
 	for {
+		// ambil job dari antrean, list reservation:queue hilang 1 persatu
 		job, err := rdb.BLPop(ctx, 0*time.Second, "reservation:queue").Result()
 		if err != nil {
 			fmt.Println("Error reading queue:", err)
 			continue
 		}
 
+		// mengambil data request dari redis yang telah dipush dengan marshal
 		var request models.ReservationRequest
-		if err := json.Unmarshal([]byte(job[1]), &request); err != nil {
+		if err := json.Unmarshal([]byte(job[1]), &request); err != nil { // job[1] adalah data request
 			fmt.Println("Invalid job data:", err)
 			continue
 		}
